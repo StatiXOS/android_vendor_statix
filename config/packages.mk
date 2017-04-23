ca564c1b32aece95c1bbb76cafbda44e8c5f22c4
xplodwild <xplod@ouverta.fr>
2017-04-23 08:14:21 +0200

    Add basic vendor

---
+++ config/packages.mk
@@ -0,0 +1,12 @@
+# Required packages
+PRODUCT_PACKAGES += \
+    Basic \
+    CellBroadcastReceiver \
+    LatinIME
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    openvpn \
+    e2fsck \
+    mke2fs \
+    tune2fs
