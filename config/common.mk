e1258a44330b07cb598ffe62aab42702fbb9e1ef
Josh Chasky <jchasky@gmail.com>
2017-09-10 12:41:51 -0400

    Fix AOSP gesture typing
    Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec303

--- config/common.mk
+++ config/common.mk
@@ -21,14 +21,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
 # LatinIME gesture typing
-ifeq ($(TARGET_ARCH),arm64)
-PRODUCT_COPY_FILES += \
-    vendor/statix/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/statix/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-else
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
     vendor/statix/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/statix/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/statix/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/statix/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
 # Fix Google dialer
