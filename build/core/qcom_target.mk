0b51f176a5102005fe9af3ba4bf9611af6ed376c
Luca Stefani <luca020400@lineageos.org>
2017-09-21 01:55:31 -0400

    Enable TARGET_USES_QCOM_MM_AUDIO
    Change-Id: Iff693650db4b9423bc4bb16c12f592c2a74248af

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -44,6 +44,9 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
         endif
     endif
 
+    # Allow building audio encoders
+    TARGET_USES_QCOM_MM_AUDIO := true
+
     # Enable color metadata for 8xx UM targets
     ifneq ($(filter msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
         TARGET_USES_COLOR_METADATA := true
