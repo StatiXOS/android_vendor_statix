6e43785832a13616d357ec0720be0c3ed4880a5e
Alex Cruz <du.alexcruz@gmail.com>
2017-09-22 14:12:22 -0400

    Switch paths for qcom targets
    Change-Id: I38180beb7d122a2ea6eb3abdb93dce0689897379

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -1,7 +1,7 @@
 # Target-specific configuration
 
 # Bring in Qualcomm helper macros
-include build/extras/core/qcom_utils.mk
+include vendor/extras/build/core/qcom_utils.mk
 
 # Populate the qcom hardware variants in the project pathmap.
 define ril-set-path-variant
