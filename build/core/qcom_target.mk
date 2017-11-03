efb8f52e42a4ed5044083b25ad7ec9c7d3efb945
Rashed Abdel-Tawab <rashed@linux.com>
2017-11-03 12:04:40 -0700

    statix: qcom: Set power HAL pathmap
    Change-Id: I063821f8405cafc57b37a61d119a9b8b7ef9df4a

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -94,6 +94,7 @@ $(call set-device-specific-path,GPS,gps,hardware/qcom/gps)
 $(call set-device-specific-path,SENSORS,sensors,hardware/qcom/sensors)
 $(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
+$(call set-device-specific-path,POWER,power,hardware/qcom/power)
 
 $(call wlan-set-path-variant,wlan)
 $(call bt-vendor-set-path-variant,bt)
