ab52c20f2363c7786f4e4cf06435cce48dad02e2
anayw2001 <anay1018@gmail.com>
2018-03-05 09:29:45 -0800

    Update branding.mk

--- config/branding.mk
+++ config/branding.mk
@@ -1,9 +1,9 @@
 # Set all versions
 DATE := $(shell date -u +%Y%m%d)
 STATIX_VERSION_MAJOR := 0.1-TEST
-STATIX_VERSION := $(TARGET_PRODUCT)-$(DATE)-8.1.0-$(STATIX_VERSION_MAJOR)-$(shell date -u +%H%M)
+STATIX_VERSION := $(TARGET_PRODUCT)-$(DATE)-8.1.0-$(STATIX_VERSION_MAJOR)-$(DATE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.statix.version=$(STATIX_VERSION) \
-    ro.mod.version=$(BUILD_ID)-$(DATE)
+    ro.mod.version=$(BUILD_ID)-$(DATE)-$(STATIX_VERSION_MAJOR)
