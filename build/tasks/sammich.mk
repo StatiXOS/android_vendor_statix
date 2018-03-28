ba007b89eb51ea915f20b48766eb23fdaab026e3
anayw2001 <anay1018@gmail.com>
2018-03-28 12:22:44 -0700

    vendor: gib food
    https://github.com/CarbonROM/android_build_make/blob/cr-6.1/core/Makefile
    
    thanks christian :D

---
+++ build/tasks/sammich.mk
@@ -0,0 +1,35 @@
+# This is entirely Carbon's code, from here: https://github.com/CarbonROM/android_build_make/blob/cr-6.1/core/Makefile
+
+.PHONY: asandwich 
+sandwich:
+	@echo ""
+	@echo -e ${CL_CYN}"                  Make me a sandwich.                              "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                                        "${CL_RST}
+	@echo -e ${CL_CYN}"                          |		       What? Make it yourself."${CL_RST}
+	@echo -e ${CL_CYN}"                          |                             |          "${CL_RST}
+	@echo -e ${CL_CYN}"          sudo make me a sandwich.                      |          "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                             |          "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                           Okay         "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                              \         "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                              :#.       "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                             ;\` .#      "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                             +   '      "${CL_RST}
+	@echo -e ${CL_CYN}"                          |                            \`.   '      "${CL_RST}
+	@echo -e ${CL_CYN}"                         ,,                            ,    +      "${CL_RST}
+	@echo -e ${CL_CYN}"                        #\` @                           ,\`   @      "${CL_RST}
+	@echo -e ${CL_CYN}"                       ,    #                          \`;  .,      "${CL_RST}
+	@echo -e ${CL_CYN}"                       ;    +                           :#+#       "${CL_RST}
+	@echo -e ${CL_CYN}"                    :#+;,   :                             @        "${CL_RST}
+	@echo -e ${CL_CYN}"                    #  .@\`  +                            ,+#       "${CL_RST}
+	@echo -e ${CL_CYN}"                    +   #+##.                            #+..      "${CL_RST}
+	@echo -e ${CL_CYN}"                    '   \`'#                              ;+ #      "${CL_RST}
+	@echo -e ${CL_CYN}"                    ,    +#.,@::++                     .. #  ;.    "${CL_RST}
+	@echo -e ${CL_CYN}"                    .    @    \`,@ #                    #  :\`       "${CL_RST}
+	@echo -e ${CL_CYN}"                    \`     ,#@#+#+'                       ; #       "${CL_RST}
+	@echo -e ${CL_CYN}"                    .            .#                     .  \`.      "${CL_RST}
+	@echo -e ${CL_CYN}"                    \`            ,\`,                    +   #      "${CL_RST}
+	@echo -e ${CL_CYN}"                    :     \`\`\` \`  ; #                    ;   +      "${CL_RST}
+	@echo -e ${CL_CYN}"                     @@#;,...,:@@,  \`                   ,   ,\`     "${CL_RST}
+	@echo -e ${CL_CYN}"                      #,       #'                                  "${CL_RST}
+	@echo -e ${CL_CYN}"                      \`        \`                                   "${CL_RST}
+measandwich: sandwich
