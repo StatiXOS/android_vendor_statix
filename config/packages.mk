63ed86cc925fe2420a07e04ae61519ab9ddc0452
Kshitij Gupta <kshitijgm@gmail.com>
2018-04-26 09:21:02 +0000

    vendor: Build overlays and change base labels
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -6,20 +6,13 @@ PRODUCT_PACKAGES += \
 
 # Extra tools
 PRODUCT_PACKAGES += \
-<<<<<<< HEAD
     openvpn \
     e2fsck \
     mke2fs \
     tune2fs
-=======
-    Phonograph \
-    Turbo
 
-# DU Utils Library
 PRODUCT_PACKAGES += \
-    org.dirtyunicorns.utils
-
-PRODUCT_BOOT_JARS += \
-    org.dirtyunicorns.utils
-
->>>>>>> 88b66f34... add in package size when build finishes
+    PixelTheme \
+    StockTheme \
+    PixelBaseTheme \
+    StockBaseTheme
