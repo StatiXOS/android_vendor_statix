a187296226561610c22e7eada3cebbdb13164a2c
Anay Wadhera <anay1018@gmail.com>
2018-04-28 15:00:38 -0700

    fix stuffes

--- config/branding.mk
+++ config/branding.mk
@@ -1,10 +1,12 @@
 # Set all versions
 DATE := $(shell date -u +%Y%m%d)
 TIME := $(shell date -u +%H%M)
+ifndef STATIX_BUILD_TYPE
+       STATIX_BUILD_TYPE := UNOFFICIAL
+endif
 STATIX_VERSION_MAJOR := 0.1-TEST
 STATIX_VERSION := $(TARGET_PRODUCT)-$(DATE)-$(TIME)-8.1.0-$(STATIX_VERSION_MAJOR)-$(STATIX_BUILD_TYPE)
 
-STATIX_BUILD_TYPE := UNOFFICIAL
 ifeq ($(filter-out OFFICIAL,$(STATIX_BUILD_TYPE)),)
     PRODUCT_DEFAULT_DEV_CERTIFICATE := ../.keys/releasekey
 endif
