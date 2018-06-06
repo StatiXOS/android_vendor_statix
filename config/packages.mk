3737a5598aecdb748e23d21f6b9d5b4488fc6337
Kshitij Gupta <kshitijgm@gmail.com>
2018-06-06 15:31:30 +0000

    vendor: Re-generate accent packages
    - Using shades 700, 600 and 500 from https://www.materialui.co/colors
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -12,12 +12,26 @@ PRODUCT_PACKAGES += \
     tune2fs
 
 PRODUCT_PACKAGES += \
-    PixelTheme \
-    StockTheme \
+    AmberTheme \
+    BlueTheme \
+    CyanTheme \
+    DeepOrangeTheme \
+    DeepPurpleTheme \
+    GreenTheme \
+    IndigoTheme \
+    LightBlueTheme \
+    LightGreenTheme \
+    LimeTheme \
+    OrangeTheme \
     PinkTheme \
     PixelBaseTheme \
+    PixelTheme \
+    PurpleTheme \
+    RedTheme \
     StockBaseTheme \
-    StockBaseFixedTheme
+    StockFixedBaseTheme \
+    StockTheme \
+    YellowTheme
 
 # Dark/BlackUI Packages
 PRODUCT_PACKAGES += \
