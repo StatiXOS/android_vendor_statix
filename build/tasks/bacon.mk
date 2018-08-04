cffb9460cb2d73263a9946f49205c836ca01a1ad
anayw2001 <anay1018@gmail.com>
2018-08-04 22:26:20 +0000

    I like this better.

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -22,5 +22,27 @@ STATIX_TARGET_PACKAGE := $(PRODUCT_OUT)/$(STATIX_VERSION).zip
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(STATIX_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(STATIX_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(STATIX_TARGET_PACKAGE).md5sum
+	@echo " "
+	@echo " "
+	@echo "                                                                                                            "
+	@echo "                                                                                                            "
+	@echo "                          tttt                                    tttt            iiii                      "
+	@echo "                       ttt:::t                                 ttt:::t           i::::i                     "
+	@echo "                       t:::::t                                 t:::::t            iiii                      "
+	@echo "                       t:::::t                                 t:::::t                                      "
+	@echo "    ssssssssss   ttttttt:::::ttttttt      aaaaaaaaaaaaa  ttttttt:::::ttttttt    iiiiiii xxxxxxx      xxxxxxx"
+	@echo "  ss::::::::::s  t:::::::::::::::::t      a::::::::::::a t:::::::::::::::::t    i:::::i  x:::::x    x:::::x "
+	@echo "ss:::::::::::::s t:::::::::::::::::t      aaaaaaaaa:::::at:::::::::::::::::t     i::::i   x:::::x  x:::::x  "
+	@echo "s::::::ssss:::::stttttt:::::::tttttt               a::::atttttt:::::::tttttt     i::::i    x:::::xx:::::x   "
+	@echo " s:::::s  ssssss       t:::::t              aaaaaaa:::::a      t:::::t           i::::i     x::::::::::x    "
+	@echo "   s::::::s            t:::::t            aa::::::::::::a      t:::::t           i::::i      x::::::::x     "
+	@echo "      s::::::s         t:::::t           a::::aaaa::::::a      t:::::t           i::::i      x::::::::x     "
+	@echo "ssssss   s:::::s       t:::::t    tttttta::::a    a:::::a      t:::::t    tttttt i::::i     x::::::::::x    "
+	@echo "s:::::ssss::::::s      t::::::tttt:::::ta::::a    a:::::a      t::::::tttt:::::ti::::::i   x:::::xx:::::x   "
+	@echo "s::::::::::::::s       tt::::::::::::::ta:::::aaaa::::::a      tt::::::::::::::ti::::::i  x:::::x  x:::::x  "
+	@echo " s:::::::::::ss          tt:::::::::::tt a::::::::::aa:::a       tt:::::::::::tti::::::i x:::::x    x:::::x "
+	@echo "  sssssssssss              ttttttttttt    aaaaaaaaaa  aaaa         ttttttttttt  iiiiiiiixxxxxxx      xxxxxxx"
+	@echo " " 
+	@echo " "
 	@echo "Package Complete: $(STATIX_TARGET_PACKAGE)" >&2
 	@echo "Package size: `du -h $(STATIX_TARGET_PACKAGE) | cut -f 1`"
