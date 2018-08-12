081512425f1476a2cc4b17d1194710820b556120
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-12 10:51:27 -0700

    vendor: Exclude all custom overlays from RRO
    None of this is device specific and therefore needs to be excluded from
    RRO overlay packages
    
    Change-Id: I24d882b783a8d7c9e494298e2c26c8ef689a515a

--- config/common.mk
+++ config/common.mk
@@ -65,4 +65,5 @@ include vendor/statix/config/pixel2-audio_prebuilt.mk
 include vendor/statix/config/statix-bootanimation.mk
 
 # Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/statix/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/statix/overlay/common
