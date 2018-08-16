a191bafb0a3759f3c0b5f343368255189cb0fab7
Alex Cruz <alex@dirtyunicorns.com>
2018-08-16 15:42:04 -0500

    vendorsetup: Make sure we generate the correct devices for the lunch menu
    Simple and to the point, we grep the manifest to check what branch we're
    on and we generate devices onto the lunch menu based on that. Nothing
    fancy but it works.
    
    Change-Id: Id1dc2589067f20fe343fa3d0ed601eb14f7401b9

---
+++ vendorsetup.sh
@@ -0,0 +1,17 @@
+aosp_devices=('dragon' 'marlin' 'shamu' 'taimen')
+caf_devices=('cheeseburger' 'dumpling' 'nash' 'oneplus3' 'potter' 'tenderloin')
+
+function lunch_devices() {
+    add_lunch_combo du_${device}-user
+    add_lunch_combo du_${device}-userdebug
+}
+
+if [[ $( grep -i "caf" manifest/README.md) ]]; then
+    for device in ${caf_devices[@]}; do
+        lunch_devices
+    done
+else
+    for device in ${aosp_devices[@]}; do
+        lunch_devices
+    done
+fi
