ec7a7f33a3b521f0e84e0fd26826f8e7cadd307c
Anay Wadhera <anay1018@gmail.com>
2018-08-22 05:51:13 +0000

    statix: set a default accent

--- config/common.mk
+++ config/common.mk
@@ -8,7 +8,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.boot.vendor.overlay.theme=com.google.android.theme.pixel \
+    ro.boot.vendor.overlay.theme=com.potato.overlay.accent.Red \
     ro.build.selinux=1
 
 # Fix Google dialer
