4248df197201b37ddb00198ef92db70a5e2fecc7
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -1,5 +1,3 @@
-# Target-specific configuration
-
 # Bring in Qualcomm helper macros
 include vendor/extras/build/core/qcom_utils.mk
 
@@ -29,64 +27,6 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 endif
 
 ifeq ($(BOARD_USES_QTI_HARDWARE),true)
-
-    A_FAMILY := msm7x27a msm7x30 msm8660 msm8960
-    B_FAMILY := msm8226 msm8610 msm8974
-    B64_FAMILY := msm8992 msm8994
-    BR_FAMILY := msm8909 msm8916
-    UM_FAMILY := msm8937 msm8953 msm8996
-
-    BOARD_USES_ADRENO := true
-
-    TARGET_USES_QCOM_BSP := true
-
-    # Tell HALs that we're compiling an AOSP build with an in-line kernel
-    TARGET_COMPILE_WITH_MSM_KERNEL := true
-
-    ifneq ($(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
-        # Enable legacy audio functions
-        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-            ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-                USE_CUSTOM_AUDIO_POLICY := 1
-            endif
-        endif
-    endif
-
-    # Allow building audio encoders
-    TARGET_USES_QCOM_MM_AUDIO := true
-
-    # Enable color metadata for 8xx UM targets
-    ifneq ($(filter msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
-        TARGET_USES_COLOR_METADATA := true
-    endif
-
-    # List of targets that use master side content protection
-    MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660
-
-    # Every qcom platform is considered a vidc target
-    MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
-
-    ifeq ($(call is-board-platform-in-list, $(A_FAMILY)),true)
-        QCOM_HARDWARE_VARIANT := msm8960
-    else
-    ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
-        QCOM_HARDWARE_VARIANT := msm8974
-    else
-    ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
-        QCOM_HARDWARE_VARIANT := msm8994
-    else
-    ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
-        QCOM_HARDWARE_VARIANT := msm8916
-    else
-    ifeq ($(call is-board-platform-in-list, $(UM_FAMILY)),true)
-        QCOM_HARDWARE_VARIANT := msm8996
-    else
-        QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
-    endif
-    endif
-    endif
-    endif
-    endif
 
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
