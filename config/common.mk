DirtyUnicorns/android_vendor_du

Add branding.mk
Change-Id: Ic9848e13ddb4edda19d2e68ae1913b9d84e2be8b

config/common.mk
@@ -32,3 +32,6 @@ PRODUCT_COPY_FILES += \
 
 # Packages
 include vendor/du/config/packages.mk
+
+# Branding
+include vendor/du/config/branding.mk

StatiXOS/android_vendor_statix

config/common.mk
@@ -20,3 +20,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Packages
 include vendor/statix/config/packages.mk
+
+# Branding
+include vendor/statix/config/branding.mk