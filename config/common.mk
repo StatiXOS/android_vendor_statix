DirtyUnicorns/android_vendor_du

Bring backup tool back
Change-Id: Ic2785d880a55d98fa9b3d7be3cf6f73483fe3a40

config/common.mk
@@ -50,10 +50,10 @@ PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
 # Backup Tool
-#PRODUCT_COPY_FILES += \
-#    vendor/extras/build/tools/backuptool.sh:install/bin/backuptool.sh \
-#    vendor/extras/build/tools/backuptool.functions:install/bin/backuptool.functions \
-#    vendor/extras/build/tools/50-du.sh:system/addon.d/50-du.sh
+PRODUCT_COPY_FILES += \
+    vendor/extras/build/tools/backuptool.sh:install/bin/backuptool.sh \
+    vendor/extras/build/tools/backuptool.functions:install/bin/backuptool.functions \
+    vendor/extras/build/tools/50-du.sh:system/addon.d/50-du.sh
 
 # Packages
 include vendor/du/config/packages.mk

StatiXOS/android_vendor_statix

config/common.mk
@@ -41,6 +41,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/statix/prebuilt/bin/clean_cache.sh:system/bin/clean_cache.sh
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/statix/build/tools/backuptool.sh:install/bin/backuptool.sh \
+    vendor/statix/build/tools/backuptool.functions:install/bin/backuptool.functions \
+    vendor/statix/build/tools/50-statix.sh:system/addon.d/50-statix.sh
+
 # Packages
 include vendor/statix/config/packages.mk
 