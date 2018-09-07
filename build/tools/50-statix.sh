5b233cac080d6399456e88f486dce06fc596368a
Anay Wadhera <anay1018@gmail.com>
2018-09-07 16:53:59 -0700

    backuptool: rebrand
    Change-Id: I857998a35ff853c6c5fe7ec37f71de0b5ec8d0c4

--- build/tools/50-statix.sh
+++ build/tools/50-statix.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
-# 
-# /system/addon.d/50-cm.sh
-# During a CM9 upgrade, this script backs up /system/etc/hosts,
+#
+# /system/addon.d/50-statix.sh
+# During an upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
