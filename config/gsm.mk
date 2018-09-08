a856aafb1b1f56fcd8ef0fad9ca2a0da758f5630
Christian Oder <myself5@carbonrom.org>
2018-09-07 21:03:35 -0700

    vendor: add initial gsm parts
    Change-Id: I561ce97ede9a4b78d2c9f1a3ecb57af120d106be

---
+++ config/gsm.mk
@@ -0,0 +1,10 @@
+# GSM APN list
+PRODUCT_COPY_FILES += \
+    vendor/statix/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+PRODUCT_COPY_FILES += \
+    vendor/statix/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
