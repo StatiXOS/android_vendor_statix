c62aa1c14a832646b5e51db5c933a0f503d90a91
Jertlok <jertlokk@gmail.com>
2018-09-14 16:29:43 +0000

    repopick: Electrify
    Change-Id: Ie7e9e0affbab38425c772b186d2c689d013e7f95

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -122,7 +122,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to Gerrit
-    default_gerrit = 'http://gerrit.dirtyunicorns.com'
+    default_gerrit = 'http://review.statixos.com'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
@@ -216,7 +216,7 @@ if __name__ == '__main__':
     #{project: {path, revision}}
 
     for project in projects:
-        name = project.get('name').replace("DirtyUnicorns/", "")
+        name = project.get('name').replace("StatiXOS/", "")
         path = project.get('path')
         revision = project.get('revision')
         if revision is None:
