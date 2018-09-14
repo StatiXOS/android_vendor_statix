WhyProject/android_vendor_extras

Change repopick path

build/envsetup.sh
@@ -74,7 +74,7 @@ function pushboot() {
 function repopick() {
     set_stuff_for_environment
     T=$(gettop)
-    $T/build/extras/tools/repopick.py $@
+    $T/vendor/extras/build/tools/repopick.py $@
 }
 
 function aospremote() {

StatiXOS/android_vendor_statix

Change-Id: Iefa219217e9fdb1ed12371c97e7ca0e7d98e4942

build/envsetup.sh
@@ -65,5 +65,5 @@ function brunch()
 function repopick() {
     set_stuff_for_environment
     T=$(gettop)
-    $T/build/extras/tools/repopick.py $@
+    $T/vendor/statix/build/tools/repopick.py $@
 }