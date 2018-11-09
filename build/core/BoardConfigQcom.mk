a650e52f6cd31946287fe8add2dc60f50debd584
Anay Wadhera <anay1018@gmail.com>
2018-11-09 08:38:24 +0200

    Revert "build: Merge UM platforms into common HALs and disable BSP"
    This reverts commit 65c26a59f8644d222d96278d715c8de03875e20b.
    
    Change-Id: I72d46dc4b5d2410cf391f54632d89c7c41dd9dff

--- build/core/BoardConfigQcom.mk
+++ build/core/BoardConfigQcom.mk
@@ -5,15 +5,11 @@ A_FAMILY := msm7x27a msm7x30 msm8660 msm8960
 B_FAMILY := msm8226 msm8610 msm8974
 B64_FAMILY := msm8992 msm8994
 BR_FAMILY := msm8909 msm8916
-UM_3_18_FAMILY := msm8937 msm8953 msm8996
-UM_4_4_FAMILY := msm8998 sdm660
+UM_FAMILY := msm8937 msm8953 msm8996
 
 BOARD_USES_ADRENO := true
 
-# UM platforms no longer need this set on O+
-ifneq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY) $(UM_4_4_FAMILY)),true)
-    TARGET_USES_QCOM_BSP := true
-endif
+TARGET_USES_QCOM_BSP := true
 
 # Tell HALs that we're compiling an AOSP build with an in-line kernel
 TARGET_COMPILE_WITH_MSM_KERNEL := true
@@ -21,48 +17,42 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 ifneq ($(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
     # Enable legacy audio functions
     ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-        USE_CUSTOM_AUDIO_POLICY := 1
+        ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+            USE_CUSTOM_AUDIO_POLICY := 1
+        endif
     endif
 endif
 
 # Allow building audio encoders
 TARGET_USES_QCOM_MM_AUDIO := true
 
-# Enable color metadata for modern UM targets
-ifneq ($(filter msm8996 msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
+# Enable color metadata for 8xx UM targets
+ifneq ($(filter msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     TARGET_USES_COLOR_METADATA := true
 endif
 
 # List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660
 
+# Every qcom platform is considered a vidc target
+MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
+
 ifeq ($(call is-board-platform-in-list, $(A_FAMILY)),true)
-    MSM_VIDC_TARGET_LIST := $(A_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8960
 else
 ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
-    MSM_VIDC_TARGET_LIST := $(B_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8974
 else
 ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
-    MSM_VIDC_TARGET_LIST := $(B64_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8994
 else
 ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
-    MSM_VIDC_TARGET_LIST := $(BR_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8916
 else
-ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY)),true)
-    MSM_VIDC_TARGET_LIST := $(UM_3_18_FAMILY)
+ifeq ($(call is-board-platform-in-list, $(UM_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8996
 else
-ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
-    MSM_VIDC_TARGET_LIST := $(UM_4_4_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8998
-else
-    MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
     QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
-endif
 endif
 endif
 endif
