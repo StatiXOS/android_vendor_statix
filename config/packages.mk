399862507e8a11d566a6eb905b7ecbdff61d850a
wirmpolter <wirmpolter@gmail.com>
2018-11-27 12:19:46 -0500

    vendor: packages: Build WeatherProvider
    Change-Id: I22328305e268ba230d422bcfd21d56abb3fff803

--- config/packages.mk
+++ config/packages.mk
@@ -1,3 +1,12 @@
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
+
+# StatiX Packages
+PRODUCT_PACKAGES += \
+    WeatherProvider
+
+# WeatherProvider
+PRODUCT_COPY_FILES += \
+    vendor/statix/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
+    vendor/statix/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
