c60fe73f67f8c5608269a9368255c01a3d6281f5
wirmpolter <wirmpolter@gmail.com>
2018-11-27 12:19:46 -0500

    vendor/statix: Build WeatherProvider
    Change-Id: I22328305e268ba230d422bcfd21d56abb3fff803

--- config/packages.mk
+++ config/packages.mk
@@ -59,4 +59,11 @@ PRODUCT_PACKAGES += \
     turbo.xml \
     privapp-permissions-turbo.xml
 
+# StatiX Packages
+PRODUCT_PACKAGES += \
+    WeatherProvider
 
+# WeatherProvider
+PRODUCT_COPY_FILES += \
+    vendor/statix/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
+    vendor/statix/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
