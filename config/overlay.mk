faf44f573aa67aa8b4622dea09484bd207e71633
ElDainosor <eldainosor@gmail.com>
2018-12-09 17:24:45 -0600

    statix: move overlays into their own makefile
    Change-Id: I73e0b6ab3d163c1f286db3f90fa5456888d0ba54

---
+++ config/overlay.mk
@@ -0,0 +1,48 @@
+# Accent packages
+PRODUCT_PACKAGES += \
+    AmberTheme \
+    BlueTheme \
+    CyanTheme \
+    DeepOrangeTheme \
+    DeepPurpleTheme \
+    GreenTheme \
+    IndigoTheme \
+    LightBlueTheme \
+    LightGreenTheme \
+    LimeTheme \
+    OrangeTheme \
+    PinkTheme \
+    PixelBaseTheme \
+    PixelTheme \
+    PurpleTheme \
+    RedTheme \
+    StockBaseTheme \
+    StockFixedBaseTheme \
+    TealTheme \
+    YellowTheme \
+    UserOne \
+    UserTwo \
+    UserThree \
+    UserFour \
+    UserFive \
+    UserSix \
+    UserSeven
+
+# Dark/BlackUI Packages
+PRODUCT_PACKAGES += \
+    GBoardDarkTheme \
+    SettingsDarkTheme \
+    SystemDarkTheme \
+    SysuiQsDarkTheme \
+    SettingsBlackTheme \
+    SystemBlackTheme \
+    SystemSettingsIconTint \
+    SysuiQsBlackTheme \
+
+# QS Theme Packages
+PRODUCT_PACKAGES += \
+    FramedQS \
+    SplitQS \
+    SuperBubbleQS \
+    TeardropQS \
+    HexagonQS \
--- config/packages.mk
+++ config/packages.mk
@@ -1,53 +1,3 @@
-# Accent packages
-PRODUCT_PACKAGES += \
-    AmberTheme \
-    BlueTheme \
-    CyanTheme \
-    DeepOrangeTheme \
-    DeepPurpleTheme \
-    GreenTheme \
-    IndigoTheme \
-    LightBlueTheme \
-    LightGreenTheme \
-    LimeTheme \
-    OrangeTheme \
-    PinkTheme \
-    PixelBaseTheme \
-    PixelTheme \
-    PurpleTheme \
-    RedTheme \
-    StockBaseTheme \
-    StockFixedBaseTheme \
-    TealTheme \
-    YellowTheme \
-    UserOne \
-    UserTwo \
-    UserThree \
-    UserFour \
-    UserFive \
-    UserSix \
-    UserSeven
-
-# Dark/BlackUI Packages
-PRODUCT_PACKAGES += \
-    GBoardDarkTheme \
-    SettingsDarkTheme \
-    SystemDarkTheme \
-    SysuiQsDarkTheme \
-    SettingsBlackTheme \
-    SystemBlackTheme \
-    SystemSettingsIconTint \
-    SysuiQsBlackTheme \
-
-# QS Theme Packages
-PRODUCT_PACKAGES += \
-    FramedQS \
-    SplitQS \
-    SuperBubbleQS \
-    TeardropQS \
-    HexagonQS \
-
-
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
@@ -71,3 +21,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/statix/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
     vendor/statix/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
+
+-include vendor/statix/config/overlay.mk
