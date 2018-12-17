a45525f80775e7e4d47eefc8f538f4c956760b9b
Anay Wadhera <anay1018@gmail.com>
2018-12-16 23:02:07 -0800

    statix: update APNs from crosshatch december factory image
    Change-Id: I6b6220655650ae4d54266aacb228099ac683d9ad

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,6 +1,6 @@
 # GSM APN list
 PRODUCT_COPY_FILES += \
-    vendor/statix/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/statix/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 PRODUCT_COPY_FILES += \
     vendor/statix/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
