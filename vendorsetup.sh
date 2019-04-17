94b15eb9c36eb5dfbe7861244749860797adf511
ZVNexus <zvnexus@outlook.com>
2019-04-17 12:17:09 -0400

    statix: fix lunch now that we introduced CAF telephony to AOSP
    Change-Id: I6f145018bd205b465ea94ada4b9177a09eb17a7f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ function lunch_devices() {
     add_lunch_combo statix_${device}-userdebug
 }
 
-if [[ $( grep -i "caf" .repo/manifests/include.xml) ]]; then
+if [[ $( grep -i "external/json-c" .repo/manifests/include.xml) ]]; then
     for device in ${caf_devices[@]}; do
         lunch_devices
     done
