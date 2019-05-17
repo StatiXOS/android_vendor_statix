5554715aa03683850274e30a2148c9fad83c4a05
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-17 12:03:57 -0700

    qcom: Add UM 4.14 platforms
    * Note: msmnile is treated like msmnile because unlike other Qualcomm
      codenames msmnile does not expand to 8150.
    
    Change-Id: If246c0ec06de22097d369c17ce48aa1270bab19c

--- build/core/ProductConfigQcom.mk
+++ build/core/ProductConfigQcom.mk
@@ -8,7 +8,8 @@ BR_FAMILY := msm8909 msm8916
 UM_3_18_FAMILY := msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY)
+UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET)
+UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)
 
 BOARD_USES_ADRENO := true
 
@@ -38,12 +39,12 @@ ifeq ($(call is-board-platform-in-list, $(UM_PLATFORMS)),true)
 endif
 
 # Enable DRM PP driver on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)),true)
     TARGET_USES_DRM_PP := true
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
+MASTER_SIDE_CP_TARGET_LIST := msm8996 $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)
 
 # Every qcom platform is considered a vidc target
 MSM_VIDC_TARGET_LIST := $(PRODUCT_BOARD_PLATFORM)
@@ -69,7 +70,11 @@ else
 ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := sdm845
 else
+ifeq ($(call is-board-platform-in-list, $(UM_4_14_FAMILY)),true)
+    QCOM_HARDWARE_VARIANT := sm8150
+else
     QCOM_HARDWARE_VARIANT := $(PRODUCT_BOARD_PLATFORM)
+endif
 endif
 endif
 endif
