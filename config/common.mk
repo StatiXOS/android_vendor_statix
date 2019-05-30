1a1879b877d48461297937fe479a58b3dbfce179
Anay Wadhera <anay1018@gmail.com>
2019-05-30 08:22:33 -0700

    statix: set opengl renderer for all devices
    this should result in fewer frame drops
    
    Change-Id: Ic5f0c2a3d77f43d32d7b5b7cb9cf7ea6a98c6db9

--- config/common.mk
+++ config/common.mk
@@ -19,7 +19,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.com.android.dateformat=MM-dd-yyyy \
     persist.sys.disable_rescue=true \
-    ro.build.selinux=1
+    ro.build.selinux=1 \
+    debug.hwui.renderer=opengl
 
 # LatinIME gesture typing
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
