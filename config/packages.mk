a15e0d988ef9c09e0e2d14da5e9eee618cb83095
martincz <martincz@mokeedev.com>
2019-08-24 01:01:56 +0800

    vendor: Build Longshot app
    Change-Id: Ib0df2f4c8991b55eb0dd57fef0ce91bc94b4f05a

--- config/packages.mk
+++ config/packages.mk
@@ -5,11 +5,12 @@ PRODUCT_PACKAGES += \
 # StatiX Packages
 PRODUCT_PACKAGES += \
     CustomDoze \
+    Longshot \
     ThemePicker \
     WeatherProvider
 
 PRODUCT_PACKAGES += \
-     StatixOverlayStub
+    StatixOverlayStub
 
 # Charger images
 PRODUCT_PACKAGES += \
