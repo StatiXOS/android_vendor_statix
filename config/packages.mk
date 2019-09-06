7aad72a7d7b6bde71c5514809533ad93c00f860c
Anay Wadhera <anay1018@gmail.com>
2019-09-06 16:26:39 -0700

    statix: comment out some packages

--- config/packages.mk
+++ config/packages.mk
@@ -4,23 +4,23 @@ PRODUCT_PACKAGES += \
 
 # Packages
 # Turbo
-PRODUCT_PACKAGES += \
-    Turbo \
-    turbo.xml \
-    privapp-permissions-turbo.xml
+#PRODUCT_PACKAGES += \
+#    Turbo \
+#    turbo.xml \
+#    privapp-permissions-turbo.xml
 
 # StatiX Packages
-PRODUCT_PACKAGES += \
-    WeatherProvider \
-    CustomDoze
+#PRODUCT_PACKAGES += \
+#    WeatherProvider \
+#    CustomDoze
 
 # Fonts packages
-PRODUCT_PACKAGES += \
-    statix-fonts
+#PRODUCT_PACKAGES += \
+#    statix-fonts
 
 # WeatherProvider
-PRODUCT_COPY_FILES += \
-    vendor/statix/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
-    vendor/statix/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
+#PRODUCT_COPY_FILES += \
+#    vendor/statix/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
+#    vendor/statix/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
 
 #-include vendor/statix/config/overlay.mk
