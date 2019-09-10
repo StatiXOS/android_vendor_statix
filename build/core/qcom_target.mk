0fca7d4f6fb71db3b52a3b6339508e629a990d5c
Andrew Ozahoski <aozahoski@gmail.com>
2019-09-10 14:48:31 -0400

    use aosp wlan and custom bt

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -36,11 +36,9 @@ $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource
 $(call set-device-specific-path,POWER,power,hardware/qcom/power)
 
 $(call ril-set-path-variant,ril)
-$(call wlan-set-path-variant,wlan-caf)
-$(call bt-vendor-set-path-variant,bt-caf)
+$(call wlan-set-path-variant,wlan)
+$(call bt-vendor-set-path-variant,bt)
 
-PRODUCT_CFI_INCLUDE_PATHS += \
-    hardware/qcom/wlan-caf/qcwcn/wpa_supplicant_8_lib
 else
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio)
