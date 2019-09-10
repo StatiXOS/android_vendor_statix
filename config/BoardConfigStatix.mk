7022cb5807cdfb716351bd555174eebd4451947d
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-09 20:52:36 -0400

    vendor: statix: Refactor BoardConfigQcom to ProductConfigQcom
    - Multiple variables are now readonly, since they need to be exported
      for other scripts early on. These variables shouldn't be set in
      BoardConfig* scripts anymore, and refactored + included in product
      configs. Due to this, various BOARD_* vars are now refactored to
      PRODUCT_*. This includes the following:
        BOARD_USES_QCOM_HARDWARE -> PRODUCT_USES_QCOM_HARDWARE
        TARGET_BOARD_PLATFORM -> PRODUCT_BOARD_PLATFORM
    - Note, TARGET_BOARD_PLATFORM is still being used in other places, but
      both vars have the same value.
    
    Change-Id: I42321e3ec466465350d06c7d46b240644a36efb6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/BoardConfigStatix.mk
+++ config/BoardConfigStatix.mk
@@ -1,7 +1,2 @@
 include vendor/statix/config/BoardConfigKernel.mk
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/statix/build/core/BoardConfigQcom.mk
-endif
-
 include vendor/statix/config/BoardConfigSoong.mk
