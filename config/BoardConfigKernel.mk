PotatoProject/vendor_potato

BoardConfigKernel: Load origpath for ccache exec
- ccache is a path tool and is not allowed. In our case, it isn't a big
  deal, given we're compiling kernel inline. Load origpath to avoid the
  error and load the system ccache exec.

Change-Id: I16578442d75ee17e644749fe85ef0fb50b150977
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

config/BoardConfigKernel.mk
@@ -236,7 +236,7 @@ endif
 
 ifneq ($(USE_CCACHE),)
     # Detect if the system already has ccache installed to use instead of the prebuilt
-    ccache := $(shell which ccache)
+    ccache := $(shell PATH=$(shell cat $(OUT_DIR)/.path_interposer_origpath):$$PATH which ccache)
 
     ifeq ($(ccache),)
         ccache := $(BUILD_TOP)/prebuilts/misc/$(HOST_PREBUILT_TAG)/ccache/ccache

StatiXOS/android_vendor_statix

config/BoardConfigKernel.mk
@@ -72,7 +72,7 @@ endif
 
 ifneq ($(USE_CCACHE),)
     # Detect if the system already has ccache installed to use instead of the prebuilt
-    CCACHE_BIN := $(shell which ccache)
+    CCACHE_BIN := $(shell PATH=$(shell cat $(OUT_DIR)/.path_interposer_origpath):$$PATH which ccache)
 
     ifeq ($(CCACHE_BIN),)
         CCACHE_BIN := $(BUILD_TOP)/prebuilts/misc/$(HOST_PREBUILT_TAG)/ccache/ccache