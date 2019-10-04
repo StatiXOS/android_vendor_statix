86a8af22ff2bf4242fdd53731acf993a001bf012
Anay Wadhera <anay1018@gmail.com>
2019-10-04 11:26:01 -0700

    statix: drop unused overlays
    Change-Id: I193115bc2aa40aa59f1ee4e3ff586ff60967d78e

--- config/overlay.mk
+++ config/overlay.mk
@@ -1,47 +1,13 @@
 # Accent packages
 PRODUCT_PACKAGES += \
     AmberTheme \
-    BlueTheme \
     CyanTheme \
-    DeepOrangeTheme \
-    DeepPurpleTheme \
     GreenTheme \
-    IndigoTheme \
-    LightBlueTheme \
-    LightGreenTheme \
     LimeTheme \
     OrangeTheme \
     PinkTheme \
-    PixelBaseTheme \
-    PixelTheme \
-    PurpleTheme \
     RedTheme \
-    StockBaseTheme \
-    StockFixedBaseTheme \
-    TealTheme \
     YellowTheme \
-    UserOne \
-    UserTwo \
-    UserThree \
-    UserFour \
-    UserFive \
-    UserSix \
-    UserSeven \
-    UserEight \
-    UserNine \
-    UserTen \
-    UserEleven \
-    UserTwelve \
-    UserThirteen
-
-# Dark/BlackUI Packages
-PRODUCT_PACKAGES += \
-    GBoardDarkTheme \
-    SettingsDarkTheme \
-    SystemDarkTheme \
-    SysuiQsDarkTheme \
-    SystemSettingsIconTint \
-    WellbeingDarkTheme \
 
 # QS Theme Packages
 PRODUCT_PACKAGES += \
