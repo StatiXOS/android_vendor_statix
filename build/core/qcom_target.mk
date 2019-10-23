d0b0c7ef8ca8800ec03e9d03b8a3199c2827c96e
Anay Wadhera <anay1018@gmail.com>
2019-10-23 12:50:02 -0700

    statix: use caf wlan
    Change-Id: Ic962c6fe7899166d094a1f1084c7aedbca2f010e

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -21,6 +21,9 @@ $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VA
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
 
-$(call set-device-specific-path,WLAN,wlan,hardware/qcom/wlan)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
 endif
