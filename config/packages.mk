8b37197ad77e0875d1d5f275695caf7576f5e76f
Anay Wadhera <anay1018@gmail.com>
2019-10-30 12:51:22 -0700

    statix: Build font overlays
    Change-Id: I6dbf20c5c02c24e06fdb41020fab6c609cd989aa

--- config/packages.mk
+++ config/packages.mk
@@ -12,3 +12,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/statix/prebuilt/common/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
     vendor/statix/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
+
+# Fonts
+PRODUCT_PACKAGES += \
+    FontArbutusSourceOverlay \
+    FontArvoLatoOverlay \
+    FontRubikRubikOverlay \
+
+-include vendor/statix/config/overlay.mk
