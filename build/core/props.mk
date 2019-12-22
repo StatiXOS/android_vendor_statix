33795d8f4b50e5c6bf99c01a39f42981d5ea54ca
Anay Wadhera <anay1018@gmail.com>
2019-12-22 00:39:18 -0800

    vendor: statix: Add build type prop
    Change-Id: I5a7e69f08f536c0d17a577690dafee152593e20d

--- build/core/props.mk
+++ build/core/props.mk
@@ -7,4 +7,5 @@ endif
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.statix.version=$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(STATIX_BASE_VERSION) \
-    ro.statix.fingerprint=$(ROM_FINGERPRINT)
+    ro.statix.fingerprint=$(ROM_FINGERPRINT) \
+    ro.statix.buildtype=$(STATIX_BUILD_TYPE)
