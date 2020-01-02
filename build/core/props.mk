a06894e82c05c0faa5b2e150a3087f336ee3339d
Volodymyr Zhdanov <wight554@gmail.com>
2020-01-01 20:18:03 -0800

    vendor: statix: Set build fingerprint for all devices
    * there's no per device way to add props to /system/build.prop
    * adding it here is the only way so far
    
    Change-Id: Iafc286d9e479677540a4ec617db76715c018ae8e

--- build/core/props.mk
+++ build/core/props.mk
@@ -1,3 +1,9 @@
+# Build fingerprint
+ifneq ($(BUILD_FINGERPRINT),)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+endif
+
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.statix.version=$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(STATIX_BASE_VERSION) \
