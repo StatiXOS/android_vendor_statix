4084ddc5ba8be21131b2e268a3c8a5930ab3c8ad
Anay Wadhera <awadhera@berkeley.edu>
2020-02-04 16:26:22 -0800

    statix: split our privapp permissions whitelist into product and system
    Change-Id: I80ea08d80b3fb1fc027cf7c5617729aa2756c1f1

--- config/common.mk
+++ config/common.mk
@@ -36,7 +36,8 @@ endif
 
 # copy privapp permissions
 PRODUCT_COPY_FILES += \
-    vendor/statix/prebuilt/common/etc/permissions/privapp-permissions-statix.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-statix.xml
+    vendor/statix/prebuilt/common/etc/permissions/privapp-permissions-statix-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-statix-product.xml \
+    vendor/statix/prebuilt/common/etc/permissions/privapp-permissions-statix-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-statix-system.xml
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
