ac1e2322d39d199f19e9bb97d7dc8bcd6b354602
ZVNexus <zvnexus@outlook.com>
2020-02-10 15:48:07 +0000

    Remove mka function.
    Serves no purpose for us, use m instead.
    
    Change-Id: Id7884f5a68fe6f8724b99478231b0ba3ff9b7fdb

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,21 +1,8 @@
 function __print_extra_functions_help() {
 cat <<EOF
 Additional functions:
-- mka:             Builds using SCHED_BATCH on all processors.
 - repopick:        Utility to fetch changes from Gerrit.
 EOF
-}
-
-# Make using all available CPUs
-function mka() {
-    case `uname -s` in
-        Darwin)
-            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-            ;;
-        *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
-            ;;
-    esac
 }
 
 function breakfast()
