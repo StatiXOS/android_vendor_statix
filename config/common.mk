TheParasiteProject/vendor_parasite_common

vendor: Exclude all custom overlays from RRO
None of this is device specific and therefore needs to be excluded from
RRO overlay packages

Change-Id: I24d882b783a8d7c9e494298e2c26c8ef689a515a

config/common.mk
@@ -123,6 +123,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aosp/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
 
 # Branding

StatiXOS/android_vendor_statix

config/common.mk
@@ -65,4 +65,5 @@ include vendor/statix/config/pixel2-audio_prebuilt.mk
 include vendor/statix/config/bootanimation.mk
 
 # Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/statix/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/statix/overlay/common