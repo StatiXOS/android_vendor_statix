da44e01c9e28324ba9c1b0291801f0d2654af2c5
Andrew Thomas <itz63c@statixos.com>
2020-03-31 13:28:30 -0400

    Revert "vendor: Build Longshot app"
    This reverts commit a15e0d988ef9c09e0e2d14da5e9eee618cb83095.
    
    Change-Id: Idda32b6a71eef645df9355f73819bed997158d6b

--- config/packages.mk
+++ config/packages.mk
@@ -5,12 +5,11 @@ PRODUCT_PACKAGES += \
 # StatiX Packages
 PRODUCT_PACKAGES += \
     CustomDoze \
-    Longshot \
     ThemePicker \
     WeatherProvider
 
 PRODUCT_PACKAGES += \
-    StatixOverlayStub
+     StatixOverlayStub
 
 # Charger images
 PRODUCT_PACKAGES += \
