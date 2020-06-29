048b6763c3657b5570920f70dae2594c6cd01065
Andrew Thomas <itz63c@statixos.com>
2020-06-29 00:19:29 -0400

    statix: add statix base version prop
    Change-Id: I9da26e0a510537a11c4196acdc8c90ae37b62be6

--- build/core/props.mk
+++ build/core/props.mk
@@ -6,6 +6,7 @@ endif
 
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.statix.version=$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
+    ro.statix.base.version=$(STATIX_BASE_VERSION) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(STATIX_BASE_VERSION) \
     ro.statix.fingerprint=$(ROM_FINGERPRINT) \
     ro.statix.buildtype=$(STATIX_BUILD_TYPE) \
