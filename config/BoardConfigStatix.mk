09ec79a5818869f29e16e7cf7ffc606f18a76199
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    config: Setup soong namespaces for TARGET_USE_QTI_BT_STACK
    To opt-in for QTI BT addons, enable TARGET_USE_QTI_BT_STACK in BoardConfig.mk
    
    Reference: [https://github.com/StatiXOS/android_vendor_qcom_opensource_bluetooth-commonsys-intf/blob/11/bt-system-opensource-product.mk]
    
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/3822
    Reviewed-by: Henrique <call190now@statixos.com>
    Tested-by: Henrique <call190now@statixos.com>

--- config/BoardConfigStatix.mk
+++ config/BoardConfigStatix.mk
@@ -1,2 +1,8 @@
+ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
+endif #TARGET_USE_QTI_BT_STACK
+
 include vendor/statix/config/BoardConfigKernel.mk
 include vendor/statix/config/BoardConfigSoong.mk
