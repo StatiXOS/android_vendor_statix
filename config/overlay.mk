88ebf468be45bc548f3cc3293f056702ff3d8ba9
Anay Wadhera <awadhera@berkeley.edu>
2020-10-02 14:07:03 -0700

    statix: add sysui overlay for black theme
    * update android overlay material_grey_900 overlay
    
    Change-Id: Ia1517b6c64c1e71306d1e8314cfa6eaafbb24a0c

--- config/overlay.mk
+++ config/overlay.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Theme
 PRODUCT_PACKAGES += \
-    StatixBlackTheme
+    StatixBlackTheme \
+    StatixBlackThemeSysui
 
 # Cutout Overlays
 ifeq ($(TARGET_HAS_NOTCH),true)
