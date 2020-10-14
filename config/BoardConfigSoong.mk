a9385ae6a2cb5c46adf4a860ff488e1223218d22
Pig <pig.priv@gmail.com>
2020-10-15 02:58:37 +0800

    soong: Add statix soong config variables
    Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables
    
    Change-Id: Icaf35b65b63d4245c92e39ce628bb8d6ffd0d4bb

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -26,3 +26,35 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += statix
+SOONG_CONFIG_statix += \
+    additional_gralloc_10_usage_bits \
+    bootloader_message_offset \
+    qcom_soong_namespace \
+    should_wait_for_qsee \
+    supports_extended_compress_format \
+    target_init_vendor_lib \
+    target_ld_shim_libs \
+    target_process_sdk_version_override \
+    target_surfaceflinger_fod_lib \
+    uses_qti_camera_device
+
+# Soong bool variables
+SOONG_CONFIG_statix_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
+SOONG_CONFIG_statix_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
+SOONG_CONFIG_statix_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+
+# Set default values
+BOOTLOADER_MESSAGE_OFFSET ?= 0
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+
+# Soong value variables
+SOONG_CONFIG_statix_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_statix_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
+SOONG_CONFIG_statix_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+SOONG_CONFIG_statix_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_statix_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_statix_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_statix_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
