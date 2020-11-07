6f83538581e4b1dd719e06198e08dd324ec3c7cf
Danny Lin <danny@kdrag0n.dev>
2020-11-07 15:09:06 -0800

    statix: Use Pixel APNs
    AOSP's APN database is missing a lot of carriers, so let's use the Pixel
    CarrierSettings APNs for better carrier compatibility out-of-the-box.
    Real Pixel devices can still use CarrierSettings to override this.
    
    Generated by carriersettings-extractor from Pixel 5 (redfin)
    RD1A.201105.003.C1.
    
    Change-Id: Ib5fad2ec2be0bbd4c62db5b822887ef9c8a769fb

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,3 +1,7 @@
+# Pixel APNs
+PRODUCT_COPY_FILES += \
+    vendor/statix/telephony/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     messaging \
