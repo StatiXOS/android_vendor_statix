25604b50ab89fca5af2f6261f2bc7cf3695409e0
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-02-06 10:46:58 -0500

    branding: Drop BUILD_TIME variable from being appended in the final zip.
    Change-Id: I480406e2f5b61040547d007e11065e053d1fa5e2

--- config/branding.mk
+++ config/branding.mk
@@ -1,19 +1,23 @@
 BUILD_DATE := $(shell date +%Y%m%d)
 BUILD_TIME := $(shell date +%H%M)
 # Versioning System
-# Use signing keys for only official
+# Set all versions
+STATIX_BASE_VERSION := v4.1
+STATIX_PLATFORM_VERSION := 11
+
+# Use signing keys and don't print date & time in the final zip, only for official builds
 ifeq ($(STATIX_BUILD_TYPE),OFFICIAL)
     PRODUCT_DEFAULT_DEV_CERTIFICATE := ./.keys/releasekey
+    STATIX_VERSION := $(TARGET_PRODUCT)-$(BUILD_DATE)-$(STATIX_PLATFORM_VERSION)-$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)
+else
+    STATIX_VERSION := $(TARGET_PRODUCT)-$(BUILD_DATE)-$(BUILD_TIME)-$(STATIX_PLATFORM_VERSION)-$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)
 endif
 
 ifndef STATIX_BUILD_TYPE
     STATIX_BUILD_TYPE := UNOFFICIAL
 endif
 
-# Set all versions
-STATIX_BASE_VERSION := v4.1
-STATIX_PLATFORM_VERSION := 11
-STATIX_VERSION := $(TARGET_PRODUCT)-$(BUILD_DATE)-$(BUILD_TIME)-$(STATIX_PLATFORM_VERSION)-$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)
+# Fingerprint
 ROM_FINGERPRINT := StatiXOS/$(PLATFORM_VERSION)/$(STATIX_BUILD_TYPE)/$(BUILD_DATE)$(BUILD_TIME)
 
 # Declare it's a StatiX build
