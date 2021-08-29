10f9fd47d5fc037fce07c4db06a3463038a996f1
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-08-29 14:33:32 -0400

    statix: Drop our custom soong header generator
    Enough significant changes have been made to soong in Android 12
    for us to continue using this without a potential rewired logic.
    On top of that, we've decided internally to use device_kernel_headers
    and qti_kernel_headers as they are gonna replace our generated_kernel_headers.
    Good bye this goes.
    
    Change-Id: I3029579ec63fc55aa7ab05ac15fa7ccf53bbc0db

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,21 +1,3 @@
-# Add variables that we wish to make available to soong here.
-ifneq (,$(wildcard $(OUT_DIR)/.path_interposer_origpath))
-ORIG_PATH := $(shell cat $(OUT_DIR)/.path_interposer_origpath)
-endif
-EXPORT_TO_SOONG := \
-    KERNEL_ARCH \
-    KERNEL_CC \
-    KERNEL_CLANG_TRIPLE \
-    KERNEL_CROSS_COMPILE \
-    KERNEL_MAKE_FLAGS \
-    TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE \
-    ORIG_PATH
-
-# Setup SOONG_CONFIG_* vars to export the vars listed above.
-# Documentation here:
-# https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
-
 SOONG_CONFIG_NAMESPACES += statixVarsPlugin
 
 SOONG_CONFIG_statixVarsPlugin :=
