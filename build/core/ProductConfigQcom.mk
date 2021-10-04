5633d13f2a8cc4ee4aa2edbd171e3af1b652b0f3
Anay Wadhera <awadhera@berkeley.edu>
2021-10-03 17:17:27 -0700

    ProductConfigQcom: Switch to AOSP device_kernel_headers library
    Determines which directory is appropriate for each chipset.
    
    Change-Id: I52e2cbd3b3c5c7b1328e5bc49166972367ce7a92

--- build/core/ProductConfigQcom.mk
+++ build/core/ProductConfigQcom.mk
@@ -80,38 +80,24 @@ MSM_VIDC_TARGET_LIST := $(PRODUCT_BOARD_PLATFORM)
 
 ifeq ($(call is-board-platform-in-list, $(A_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8960
-    TARGET_KERNEL_VERSION := 3.4
 else ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8974
-    TARGET_KERNEL_VERSION := 3.4
 else ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8994
-    TARGET_KERNEL_VERSION := 3.10
 else ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8916
 else ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8996
-    TARGET_USES_QCOM_UM_3_18_FAMILY := true
-    TARGET_KERNEL_VERSION := 3.18
 else ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8998
-    TARGET_USES_QCOM_UM_4_4_FAMILY := true
-    TARGET_KERNEL_VERSION := 4.4
 else ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := sdm845
-    TARGET_USES_QCOM_UM_4_9_FAMILY := true
-    TARGET_KERNEL_VERSION := 4.9
 else ifeq ($(call is-board-platform-in-list, $(UM_4_14_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := sm8150
-    TARGET_USES_QCOM_UM_4_14_FAMILY := true
-    TARGET_KERNEL_VERSION := 4.14
 else ifeq ($(call is-board-platform-in-list, $(UM_4_19_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := sm8250
-    TARGET_USES_QCOM_UM_4_19_FAMILY := true
-    TARGET_KERNEL_VERSION := 4.19
 else ifeq ($(call is-board-platform-in-list, $(UM_5_4_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := sm8350
-    TARGET_KERNEL_VERSION := 5.4
 else
     QCOM_HARDWARE_VARIANT := $(PRODUCT_BOARD_PLATFORM)
 endif
@@ -120,6 +106,30 @@ endif
 ifneq ($(OVERRIDE_QCOM_HARDWARE_VARIANT),)
 QCOM_HARDWARE_VARIANT := $(OVERRIDE_QCOM_HARDWARE_VARIANT)
 endif
+
+ifeq ($(call is-board-platform-in-list, $(A_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 3.4
+else ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 3.4
+else ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 3.10
+else ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 3.10
+else ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 3.18
+else ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 4.4
+else ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 4.9
+else ifeq ($(call is-board-platform-in-list, $(UM_4_14_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 4.14
+else ifeq ($(call is-board-platform-in-list, $(UM_4_19_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 4.19
+else ifeq ($(call is-board-platform-in-list, $(UM_5_4_FAMILY)),true)
+    TARGET_KERNEL_VERSION := 5.4
+endif
+
+PRODUCT_VENDOR_KERNEL_HEADERS += hardware/qcom-caf/kernel-headers/msm-$(TARGET_KERNEL_VERSION)
 
 # Required for frameworks/native
 ifeq ($(call is-board-platform-in-list, $(UM_PLATFORMS)),true)
