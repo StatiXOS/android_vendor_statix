2d8b942e5a62429ac99b9ef1c477c63a5be020bb
Anay Wadhera <awadhera@berkeley.edu>
2021-10-03 17:21:15 -0700

    statix: Remove boards that are likely not making it to S
    * 4.x+ only now.
    * upgrade 3.18 chipsets to 4.9, as nearly all of them have 4.9+ from the community.
    
    Change-Id: I69116999d3339e6415ab133ee2f51ca465d87dfc

--- build/core/ProductConfigQcom.mk
+++ build/core/ProductConfigQcom.mk
@@ -1,18 +1,13 @@
 # Bring in Qualcomm helper macros
 include vendor/statix/build/core/utils.mk
 
-A_FAMILY := msm7x27a msm7x30 msm8660 msm8960
-B_FAMILY := msm8226 msm8610 msm8974
-B64_FAMILY := msm8992 msm8994
-BR_FAMILY := msm8909 msm8916
-UM_3_18_FAMILY := msm8917 msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
-UM_4_9_FAMILY := sdm845
+UM_4_9_FAMILY := msm8917 msm8937 msm8952 msm8953 msm8996 sdm845
 UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET) $(ATOLL)
 UM_4_19_FAMILY := $(KONA) $(LITO) $(BENGAL)
 UM_5_4_FAMILY := $(LAHAINA)
 
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY)
+UM_PLATFORMS := $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY)
 QSSI_SUPPORTED_PLATFORMS := $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY)
 
 BOARD_USES_ADRENO := true
@@ -43,16 +38,6 @@ endif
 # Tell HALs that we're compiling an AOSP build with an in-line kernel
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
-ifneq ($(filter msm7x27a msm7x30 msm8660 msm8960,$(PRODUCT_BOARD_PLATFORM)),)
-    TARGET_USES_QCOM_BSP_LEGACY := true
-    # Enable legacy audio functions
-    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-        ifneq ($(filter msm8960,$(PRODUCT_BOARD_PLATFORM)),)
-            USE_CUSTOM_AUDIO_POLICY := 1
-        endif
-    endif
-endif
-
 # Allow building audio encoders
 TARGET_USES_QCOM_MM_AUDIO := true
 
@@ -78,17 +63,7 @@ MASTER_SIDE_CP_TARGET_LIST := msm8996 $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_1
 # Every qcom platform is considered a vidc target
 MSM_VIDC_TARGET_LIST := $(PRODUCT_BOARD_PLATFORM)
 
-ifeq ($(call is-board-platform-in-list, $(A_FAMILY)),true)
-    QCOM_HARDWARE_VARIANT := msm8960
-else ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
-    QCOM_HARDWARE_VARIANT := msm8974
-else ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
-    QCOM_HARDWARE_VARIANT := msm8994
-else ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
-    QCOM_HARDWARE_VARIANT := msm8916
-else ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY)),true)
-    QCOM_HARDWARE_VARIANT := msm8996
-else ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := msm8998
 else ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := sdm845
@@ -107,17 +82,7 @@ ifneq ($(OVERRIDE_QCOM_HARDWARE_VARIANT),)
 QCOM_HARDWARE_VARIANT := $(OVERRIDE_QCOM_HARDWARE_VARIANT)
 endif
 
-ifeq ($(call is-board-platform-in-list, $(A_FAMILY)),true)
-    TARGET_KERNEL_VERSION := 3.4
-else ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
-    TARGET_KERNEL_VERSION := 3.4
-else ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
-    TARGET_KERNEL_VERSION := 3.10
-else ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
-    TARGET_KERNEL_VERSION := 3.10
-else ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY)),true)
-    TARGET_KERNEL_VERSION := 3.18
-else ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
     TARGET_KERNEL_VERSION := 4.4
 else ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
     TARGET_KERNEL_VERSION := 4.9
