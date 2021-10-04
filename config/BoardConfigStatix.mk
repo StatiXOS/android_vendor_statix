66bf280bd73c226b9a813675abb23a61f1936998
Anay Wadhera <awadhera@berkeley.edu>
2021-10-03 17:48:12 -0700

    [DNM] 12 bringup temp
    Change-Id: I4bb7ee28eec1414f9f490fa7a9399235a7cfa176

--- config/BoardConfigStatix.mk
+++ config/BoardConfigStatix.mk
@@ -1,8 +1,8 @@
-ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
-endif #TARGET_USE_QTI_BT_STACK
+#ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+#PRODUCT_SOONG_NAMESPACES += \
+#    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+#    vendor/qcom/opensource/commonsys/system/bt/conf
+#endif #TARGET_USE_QTI_BT_STACK
 
 include device/statix/sepolicy/common/sepolicy.mk
 include vendor/statix/config/BoardConfigKernel.mk
