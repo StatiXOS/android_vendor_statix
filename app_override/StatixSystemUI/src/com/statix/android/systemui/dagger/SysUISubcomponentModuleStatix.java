8f5f2f49f47dc2fb44333c641689456588e58aa6
Anay Wadhera <awadhera@berkeley.edu>
2021-10-04 21:49:11 -0700

    statix: new SystemUI override
    Change-Id: I7aa765a5803d3e7a6de1ba16335eaf3f10faecc8

---
+++ app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SysUISubcomponentModuleStatix.java
@@ -0,0 +1,10 @@
+package com.statix.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {SysUIComponentStatix.class})
+public abstract class SysUISubcomponentModuleStatix {
+}
