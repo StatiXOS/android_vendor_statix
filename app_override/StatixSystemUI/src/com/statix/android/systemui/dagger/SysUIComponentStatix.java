39df06e5317be7f586490d9a8cb617933e72fb7c
Anay Wadhera <awadhera@berkeley.edu>
2021-10-04 21:49:11 -0700

    [DNM] statix: new SystemUI override
    Change-Id: I7aa765a5803d3e7a6de1ba16335eaf3f10faecc8

---
+++ app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SysUIComponentStatix.java
@@ -0,0 +1,25 @@
+package com.statix.android.systemui.dagger;
+
+import com.android.systemui.dagger.DefaultComponentBinder;
+import com.android.systemui.dagger.DependencyProvider;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.dagger.SystemUIBinder;
+import com.android.systemui.dagger.SysUIComponent;
+import com.android.systemui.dagger.SystemUIModule;
+
+import dagger.Subcomponent;
+
+@SysUISingleton
+@Subcomponent(modules = {
+        DefaultComponentBinder.class,
+        DependencyProvider.class,
+        SystemUIBinder.class,
+        SystemUIModule.class,
+        SystemUIStatixModule.class})
+public interface SysUIComponentStatix extends SysUIComponent {
+    @SysUISingleton
+    @Subcomponent.Builder
+    interface Builder extends SysUIComponent.Builder {
+        SysUIComponentStatix build();
+    }
+}
