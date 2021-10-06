3f8edeeb5dc7ba41f1b789036a1335ce6ccae2b5
Danny Lin <danny@kdrag0n.dev>
2021-10-05 21:00:57 -0700

    SystemUI: Use colorkt-based dynamic theme engine for Monet
    Dynamic wallpaper-based theming (Monet) is a prominent aspect of
    Android 12's Material You design system. Implement the system-wide
    wallpaper-based theming using my colorkt-based [1] dynamic theme engine [2],
    which many users prefer over Google's Pixel implementation in empirical
    tests as it typically keeps colors closer to the wallpaper and doesn't
    suffer from CAM16's blue-purple hue shift issues. It also provides
    better perceived contrast than Google's implementation in most cases
    because it uses ZCAM's modern lightness correlate as compared to CIELAB
    L* [3].
    
    NB: Unlike Google's implementation, we overlay the framework's default
    modulated surface colors for performance (no need to modulate colors
    when inflating views at runtime) and consistency (reduced dependency on
    framework-side color code, which may not respect user preferences).
    
    Demo screenshots: https://twitter.com/kdrag0n/status/1445584174790832134
    
    [1] https://github.com/kdrag0n/colorkt
    [2] https://github.com/kdrag0n/android12-extensions
    [3] https://twitter.com/MyndexResearch/status/1435491919702888448
    
    Change-Id: I48cbbacc32b2a8095eb07729aca1734f162d2391

--- app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
+++ app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
@@ -10,6 +10,8 @@ import android.os.PowerManager;
 
 import androidx.annotation.Nullable;
 
+import com.statix.android.systemui.theme.ThemeOverlayControllerStatix;
+
 import com.android.keyguard.KeyguardViewController;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dagger.qualifiers.Background;
@@ -54,6 +56,7 @@ import com.android.systemui.statusbar.policy.IndividualSensorPrivacyController;
 import com.android.systemui.statusbar.policy.IndividualSensorPrivacyControllerImpl;
 import com.android.systemui.statusbar.policy.SensorPrivacyController;
 import com.android.systemui.statusbar.policy.SensorPrivacyControllerImpl;
+import com.android.systemui.theme.ThemeOverlayController;
 
 import javax.inject.Named;
 
@@ -186,4 +189,6 @@ public abstract class SystemUIStatixModule {
     @Binds
     abstract DozeHost provideDozeHost(DozeServiceHost dozeServiceHost);
 
+    @Binds
+    abstract ThemeOverlayController provideThemeOverlayController(ThemeOverlayControllerStatix themeOverlayController);
 }
