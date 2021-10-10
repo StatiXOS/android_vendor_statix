f225c4cc31711fed82b65ce9b45acc8f1f9371e7
Anay Wadhera <anay1018@gmail.com>
2021-10-11 07:25:27 +0900

    BoardConfigStatix: include statix sepolicy
    Change-Id: I7600e4c7327c7a3a078e3ed8a7438a8d9a1a662a

--- config/BoardConfigStatix.mk
+++ config/BoardConfigStatix.mk
@@ -4,5 +4,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys/system/bt/conf
 endif #TARGET_USE_QTI_BT_STACK
 
+include device/statix/sepolicy/common/sepolicy.mk
 include vendor/statix/config/BoardConfigKernel.mk
 include vendor/statix/config/BoardConfigSoong.mk
