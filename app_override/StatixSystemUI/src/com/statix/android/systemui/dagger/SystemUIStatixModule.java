277f1932bf6615d5692f1bba3fe15d44ab161f2a
Anay Wadhera <anay1018@gmail.com>
2021-10-29 05:05:39 +0900

    StatixSystemUI: register a custom QS tile factory
    Change-Id: I146ae45039d0ca7b26808140d228d845112bf7fd

--- app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
+++ app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
@@ -10,6 +10,7 @@ import android.os.PowerManager;
 
 import androidx.annotation.Nullable;
 
+import com.statix.android.systemui.qs.tileimpl.QSFactoryImplStatix;
 import com.statix.android.systemui.theme.ThemeOverlayControllerStatix;
 
 import com.android.keyguard.KeyguardViewController;
@@ -127,7 +128,7 @@ public abstract class SystemUIStatixModule {
 
     @Binds
     @SysUISingleton
-    public abstract QSFactory bindQSFactory(QSFactoryImpl qsFactoryImpl);
+    public abstract QSFactory bindQSFactory(QSFactoryImplStatix qsFactoryImpl);
 
     @Binds
     abstract DockManager bindDockManager(DockManagerImpl dockManager);
