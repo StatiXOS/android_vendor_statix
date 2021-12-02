0915a935e0f871077ca4dbaa4a824847537c4905
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-12-02 17:32:21 -0500

    statix: Initial import for StatixSettings
    Change-Id: I2610110bc8edf9ed7f055a708895ca86ab13fd9a

---
+++ app_override/StatixSettings/src/com/google/android/settings/overlay/FeatureFactoryImplGoogle.java
@@ -0,0 +1,6 @@
+package com.google.android.settings.overlay;
+
+import com.android.settings.overlay.FeatureFactoryImpl;
+
+public final class FeatureFactoryImplGoogle extends FeatureFactoryImpl {
+}
