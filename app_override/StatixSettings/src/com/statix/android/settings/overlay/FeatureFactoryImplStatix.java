683c87cd28f7eabd21185ee51acfbd3a6af4e7c6
Anay Wadhera <awadhera@berkeley.edu>
2021-12-02 19:51:21 -0500

    StatixSettings: Import PowerUsageFeatureGoogle
    sourajitk: Implement to StatixSettings.
    
    Change-Id: I50ac0fd993eaba0e8496e7a86501b2da687e0496
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/6376
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- app_override/StatixSettings/src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
+++ app_override/StatixSettings/src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
@@ -1,6 +1,21 @@
 package com.statix.android.settings.overlay;
 
+import android.content.Context;
+
 import com.android.settings.overlay.FeatureFactoryImpl;
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImplStatix extends FeatureFactoryImpl {
+
+    private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
+
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageFeatureProvider == null) {
+            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
+                    context.getApplicationContext());
+        }
+        return mPowerUsageFeatureProvider;
+    }
 }
