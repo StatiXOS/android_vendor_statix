c9841e04f4adf1cee2fcc59c0cc1fc403919c58f
Anay Wadhera <awadhera@berkeley.edu>
2021-12-02 19:51:21 -0500

    statix: StatixSettings: Import PowerUsageFeatureGoogle
    sourajitk: Implement to StatixSettings.
    
    Change-Id: I50ac0fd993eaba0e8496e7a86501b2da687e0496

--- app_override/StatixSettings/src/com/statix/android/settings/overlay/FeatureFactoryImpl.java
+++ app_override/StatixSettings/src/com/statix/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,6 +1,15 @@
 package com.statix.android.settings.overlay;
 
 import com.android.settings.overlay.FeatureFactoryImpl;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImplStatix extends FeatureFactoryImpl {
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageFeatureProvider == null) {
+            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
+                    context.getApplicationContext());
+        }
+        return mPowerUsageFeatureProvider;
+    }
 }
