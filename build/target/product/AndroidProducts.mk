LineageOS/android_vendor_lineage

build: Add x86/x86_64 emulator targets
Change-Id: Ic8789ad2072b9ebda91f9b4a7c288a5a9bcec3fd

build/target/product/AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +28,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_tv_x86.mk \
     $(LOCAL_DIR)/lineage_tv_x86_64.mk \
     $(LOCAL_DIR)/lineage_car_arm64.mk \
-    $(LOCAL_DIR)/lineage_car_x86_64.mk
+    $(LOCAL_DIR)/lineage_car_x86_64.mk \
+    $(LOCAL_DIR)/lineage_sdk_phone_x86.mk \
+    $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_arm-userdebug \
@@ -44,4 +46,6 @@ COMMON_LUNCH_CHOICES := \
     lineage_tv_x86-userdebug \
     lineage_tv_x86_64-userdebug \
     lineage_car_arm64-userdebug \
-    lineage_car_x86_64-userdebug
+    lineage_car_x86_64-userdebug \
+    lineage_sdk_phone_x86-userdebug \
+    lineage_sdk_phone_x86_64-userdebug

StatiXOS/android_vendor_statix

build/target/product/AndroidProducts.mk
@@ -18,10 +18,14 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/statix_arm.mk \
     $(LOCAL_DIR)/statix_arm64.mk \
     $(LOCAL_DIR)/statix_x86.mk \
-    $(LOCAL_DIR)/statix_x86_64.mk
+    $(LOCAL_DIR)/statix_x86_64.mk \
+    $(LOCAL_DIR)/statix_sdk_phone_x86.mk \
+    $(LOCAL_DIR)/statix_sdk_phone_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     statix_arm-userdebug \
     statix_arm64-userdebug \
     statix_x86-userdebug \
-    statix_x86_64-userdebug
+    statix_x86_64-userdebug \
+    statix_sdk_phone_x86-userdebug \
+    statix_sdk_phone_x86_64-userdebug