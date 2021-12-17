4ffbf733e26e31ba9cb74c1d26c9a07141ecf004
jhenrique09 <jhsv09@gmail.com>
2021-12-17 10:40:44 +0900

    StatixSystemUI: port columbus (quick tap) and elmyra (squeeze) from SQ1D
    * import Google assist library/bindings and related resources as well
    
    Change-Id: I2ead6e10655937e659aa8afc8a8adc53696d51ce

--- app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SysUIComponentStatix.java
+++ app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SysUIComponentStatix.java
@@ -7,6 +7,7 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import com.statix.android.systemui.columbus.ColumbusModule;
 import com.statix.android.systemui.gamedashboard.GameDashboardModule;
 import com.statix.android.systemui.keyguard.KeyguardSliceProviderStatix;
 import com.statix.android.systemui.smartspace.KeyguardSmartspaceController;
@@ -15,6 +16,7 @@ import dagger.Subcomponent;
 
 @SysUISingleton
 @Subcomponent(modules = {
+        ColumbusModule.class,
         DefaultComponentBinder.class,
         DependencyProvider.class,
         GameDashboardModule.class,
