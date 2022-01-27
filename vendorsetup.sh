17df9a270276cea93b4c2a1743276a5e625aa2fb
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-01-27 13:03:29 -0500

    statix: Send some devices to the graveyard.
    * Another one bites the dust, they say.
    
    Change-Id: I327b1035cf13bcca0643518595eddb94d17f123b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-devices=( 'beryllium' 'cannon' 'fajita' 'gauguin' 'ginkgo' 'guacamole' 'guacamoleb' 'hotdog' 'hotdogb' 'mata' 'sunfish' 'sweet' 'TP1803' )
+devices=( 'cannon' 'guacamoleb' 'mata' 'oriole' 'sake' 'raven' 'TP1803')
 
 function lunch_devices() {
     add_lunch_combo statix_${device}-user
