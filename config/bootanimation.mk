c2e1c04ef5af6abc7f01292bc6ac4fcc0416f11a
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-02-15 16:54:33 -0500

    statix: yay new boot anim!
    Change-Id: Ie997ff29298f1b70f387e5e6e8fbee69fdd7a9ea

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -16,17 +16,15 @@
 #
 
 # Boot Animation
-scr_resolution := 1440
-statix_device := $(patsubst %f,%,$(subst statix_,,$(TARGET_PRODUCT)))
-
-ifneq ($(filter mido oneplus3,$(statix_device)),)
-scr_resolution := 1080
+ifeq ($(TARGET_SCREEN_HEIGHT),)
+TARGET_SCREEN_HEIGHT := 1920
+endif
+ifeq ($(TARGET_SCREEN_WIDTH),)
+TARGET_SCREEN_WIDTH := 1080
 endif
 
-ifneq ($(wildcard vendor/statix/bootanimation/$(scr_resolution).zip),)
-PRODUCT_COPY_FILES += \
-    vendor/statix/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
-endif
+PRODUCT_PACKAGES += \
+    bootanimation.zip
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/media/bootanimation.zip
