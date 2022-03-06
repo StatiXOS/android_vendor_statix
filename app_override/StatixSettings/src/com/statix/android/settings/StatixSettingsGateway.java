f505cc545d366541ce382e4d656859817bf2fa58
Anay Wadhera <anay1018@gmail.com>
2022-03-06 10:00:23 +0900

    StatixSettings: create StatixSettingsActivity
    * for fragments that need external launch, such as Smart Pixels
    
    Change-Id: Icf17a90758fb06aa6488d111da55d9448ada228c
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/7010
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>

---
+++ app_override/StatixSettings/src/com/statix/android/settings/StatixSettingsGateway.java
@@ -0,0 +1,11 @@
+package com.statix.android.settings;
+
+public class StatixSettingsGateway {
+
+    /**
+     * A list of fragment that can be hosted by StatixSettingsActivity. SettingsActivity will throw a
+     * security exception if the fragment it needs to display is not in this list.
+     */
+    public static final String[] ENTRY_FRAGMENTS = {
+    };
+}
