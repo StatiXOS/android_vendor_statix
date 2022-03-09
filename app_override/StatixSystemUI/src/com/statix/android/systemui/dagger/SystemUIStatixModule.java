3e29dc69796217628bbeacde873b65d39bf0a663
Fabian Kozynski <kozynski@google.com>
2022-03-09 10:19:27 +0900

    Rewrite DeviceProvisionedController
    The old version made IPC calls in the main thread. Instead, fix the
    following:
    
    1. Calls to settings provider are made in background thread, except
    maybe on object creation to set initial state or if requesting the state
    of a user that hasn't been started (unlikely).
    2. Cache state of device_provisioned and each user (that has been
    started) user_setup_complete
    3. Do all operations in background (with proper locks) and notify in
    main thread.
    4. Use UserTracker to avoid calls to .
    5. The tracker is always registered so no changes are missed, and we
    don't need to retrieve cache every time we would register. The number of
    calls from the system are pretty low any way.
    6. Listeners are not called on addCallback. None of the listeners
    expected this and at least one assumed that a call meant an actual
    change in state.
    
    Test: atest DeviceProvisionedControllerTest
    Test: manual, things are not hopelessly broken
    Fixes: 203138620
    Change-Id: I9caccd7593732a07d688d5ca2f8de8cedb8b02ef

--- app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
+++ app_override/StatixSystemUI/src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
@@ -267,9 +267,13 @@ public abstract class SystemUIStatixModule {
         return new Recents(context, recentsImplementation, commandQueue);
     }
 
-    @Binds
-    abstract DeviceProvisionedController bindDeviceProvisionedController(
-            DeviceProvisionedControllerImpl deviceProvisionedController);
+    @SysUISingleton
+    @Provides
+    static DeviceProvisionedController bindDeviceProvisionedController(
+            DeviceProvisionedControllerImpl deviceProvisionedController) {
+        deviceProvisionedController.init();
+        return deviceProvisionedController;
+    }
 
     @Binds
     abstract KeyguardViewController bindKeyguardViewController(
