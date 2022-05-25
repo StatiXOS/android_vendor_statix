30a749982c10d7799135f90445d9ca8c29d3dfe6
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-05-25 13:52:40 -0400

    statix: Beginning of general removal of obsolete stuff
    And reorganization. Add and update copyright headers where applicable
    as well.
    
    Change-Id: Iafc7b314e2101f49de151514c8b329c0e9e42816
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/7808
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- config/packages.mk
+++ config/packages.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2018-2022 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
@@ -35,9 +41,6 @@ endif
 ifeq ($(STATIX_BUILD_TYPE),OFFICIAL)
 PRODUCT_PACKAGES += \
     Updater
-else ifneq ($(AB_OTA_PARTITIONS),)
-PRODUCT_PACKAGES += \
-    LocalUpdater
 endif
 
 # Some useful shell based utilities for Android
