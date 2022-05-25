8886fba4be3b56ef45b23cd9740b92d283d06345
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-05-25 14:22:09 -0400

    statix: Reorganize stuff further
    Reorder some of the configs in common.mk. Also drop our APN configs in
    favor of the ones from ProtonAOSP.
    
    Finally, drop privapp-permissions-statix-system.xml as nothing is inside
    it anymore.
    
    Change-Id: I17bceb90f1e3e397be26005ae70c47665ecb2eda

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,10 +1,3 @@
-# GSM APN list
-PRODUCT_COPY_FILES += \
-    vendor/statix/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-
-PRODUCT_COPY_FILES += \
-    vendor/statix/prebuilt/common/etc/selective-spn-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/selective-spn-conf.xml
-
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     messaging \
