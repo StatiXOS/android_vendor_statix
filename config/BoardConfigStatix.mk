66f298a64c8459c19b57ea7614786d458ff05bc9
Anay Wadhera <anay1018@gmail.com>
2022-08-14 03:53:32 +0900

    [android13] statix: remove kernel build task
    We will move fully to an AOSP build system with prebuilt kernel
    
    Change-Id: I19a0ab0be1154780aa8b0bf46cdc1272a1efd7e1
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/8176
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- config/BoardConfigStatix.mk
+++ config/BoardConfigStatix.mk
@@ -5,5 +5,4 @@ PRODUCT_SOONG_NAMESPACES += \
 endif #TARGET_USE_QTI_BT_STACK
 
 include device/statix/sepolicy/common/sepolicy.mk
-include vendor/statix/config/BoardConfigKernel.mk
 include vendor/statix/config/BoardConfigSoong.mk
