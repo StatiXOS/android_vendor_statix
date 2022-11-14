54fd6e9b3bc96fdf9f930cda96bdcafd2635e2d0
Anay Wadhera <anay1018@gmail.com>
2022-11-13 19:08:12 -0500

    statix: fix check for picking up partner_modules
    Change-Id: Id7f5a1764e61b8841148cf8048bfa0a3a6409c9c

--- config/common.mk
+++ config/common.mk
@@ -14,7 +14,8 @@ endif
 # Define some properties for GMS
 ifneq ($(TARGET_DOES_NOT_USE_GAPPS), true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
-ifneq ($(TARGET_FLATTEN_APEX), true)
+# Anything including updatable_apex.mk should have done so by now.
+ifeq ($(TARGET_FLATTEN_APEX), false)
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules.mk)
 else
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_flatten_apex.mk)
