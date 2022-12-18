fb37e9fff4822c332a159a631a45d6096e7fdadc
blurzyy <selster02@gmail.com>
2022-12-18 10:53:22 +0000

    statix: Add updatepackage target
    - updatepackage but with proper zip name.
    - inspired by [1].
    [1]: https://github.com/yaap/vendor_yaap/commit/2bdbbe2469a4e933a8c5b80cca7b6e818219c437
    
    Change-Id: I810e1e2cfb0c140d079991893f3c0ba997408d88
    Signed-off-by: blurzyy <selster02@gmail.com>
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/9157
    Tested-by: Sourajit Karmakar <sourajitk@statixos.com>
    Reviewed-by: Sourajit Karmakar <sourajitk@statixos.com>

---
+++ build/tasks/updatepackage.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2017 Unlegacy-Android
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -----------------------------------------------------------------
+# StatiX fastboot update package
+
+STATIX_TARGET_PACKAGE := $(PRODUCT_OUT)/$(STATIX_VERSION)-img.zip
+
+.PHONY: updatepackage
+updatepackage: $(INTERNAL_UPDATE_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(STATIX_TARGET_PACKAGE)
+#	$(hide) $(MD5SUM) $(STATIX_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(STATIX_TARGET_PACKAGE).md5sum
+	@echo " "
+	@echo " "
+	@echo "                                                              :             "
+	@echo "          .                                                  t#,           ."
+	@echo "         ;W                               t                 ;##W.         ;W"
+	@echo "        f#E GEEEEEEEL         .. GEEEEEEELEj               :#L:WE        f#E"
+	@echo "      .E#f  ,;;L#K;;.        ;W, ,;;L#K;;.E#, :KW,      L .KG  ,#D     .E#f "
+	@echo "     iWW;      t#E          j##,    t#E   E#t  ,#W:   ,KG EE    ;#f   iWW;  "
+	@echo "    L##Lffi    t#E         G###,    t#E   E#t   ;#W. jWi f#.     t#i L##Lffi"
+	@echo "   tLLG##L     t#E       :E####,    t#E   E#t    i#KED.  :#G     GK tLLG##L "
+	@echo "     ,W#i      t#E      ;W#DG##,    t#E   E#t     L#W.    ;#L   LW.   ,W#i  "
+	@echo "    j#E.       t#E     j###DW##,    t#E   E#t   .GKj#K.    t#f f#:   j#E.   "
+	@echo "  .D#j         t#E    G##,,,,##,    t#E   E#t  iWf  i#K.    f#D#;  .D#j     "
+	@echo " ,WK,          t#E  :K#K:   L##,    t#E   E#t LK:    t#E     G#t  ,WK,      "
+	@echo " EG.            fE ;##D.    L##,     fE   E#t i       tDj     t   EG.       "
+	@echo " ,.              : ###,      L#,      #:   ,;.                     ,        "
+	@echo " "
+	@echo " "
+	@echo "Package Complete: $(STATIX_TARGET_PACKAGE)" >&2
+	@echo "Package size: `du -h $(STATIX_TARGET_PACKAGE) | cut -f 1`"
