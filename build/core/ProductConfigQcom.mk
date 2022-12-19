8c133cada8e0b10442949e00045f29a03a3f2a16
Anay Wadhera <anay1018@gmail.com>
2022-12-18 22:49:15 -0500

    msm-5.10: Remove def that you're not supposed to set
    Change-Id: I43dbbc2275252e6c9ad1f20e39d37e348c95df79

--- build/core/ProductConfigQcom.mk
+++ build/core/ProductConfigQcom.mk
@@ -74,7 +74,6 @@ endif
 # Enable gralloc handle support on 5.10
 ifeq ($(call is-board-platform-in-list, $(UM_5_10_FAMILY)),true)
     SOONG_CONFIG_qtidisplay_gralloc_handle_has_reserved_size := true
-    SOONG_CONFIG_qtidisplay_gralloc_handle_has_custom_content_md_reserved_size := true
 endif
 
 # Enable Gralloc4 on UM platforms that support it
