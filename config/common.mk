b2a6cbcc47e4711d8c11d2ae3bcca2bbcf27ce50
ralph950412 <ralph950412@gmail.com>
2022-12-27 00:42:14 +0800

    statix: Enable SystemUIDialog volume panel by default
    * Although the design is not yet completed, it works just fine and looks way better than the old one.
    
    Change-Id: I4ca6f01909482dbf0d245a25d8253050630e0e7d
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/9191
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -66,6 +66,10 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
 
+# Enable SystemUIDialog volume panel
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    sys.fflag.override.settings_volume_panel_in_systemui=true
+
 # Copy over some StatiX assets
 PRODUCT_COPY_FILES += \
     vendor/statix/prebuilt/etc/init.statix.rc:system/etc/init/init.statix.rc \
