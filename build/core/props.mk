a1f588c9b324d7edf4d4a962131a5a4b30e1cac8
Sourajit Karmakar <sourajitk@statixos.com>
2023-03-18 10:53:25 -0400

    branding: Stop appending time in {update|ota}packages
    * Changes to the BUILD_TIME var causes ninja to be regenerated every single
      time which is very annoying.
    
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/10346
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-by: Sourajit Karmakar <sourajitk@statixos.com>
    Tested-by: Sourajit Karmakar <sourajitk@statixos.com>
    Change-Id: Ib93cf6be990352631d7c543d7bf31970b84c8f60

--- build/core/props.mk
+++ build/core/props.mk
@@ -5,7 +5,7 @@ ADDITIONAL_BUILD_PROPERTIES += \
 endif
 
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.statix.version=$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
+    ro.statix.version=$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)-$(BUILD_DATE) \
     ro.statix.base.version=$(STATIX_BASE_VERSION) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(STATIX_BASE_VERSION) \
     ro.statix.fingerprint=$(ROM_FINGERPRINT) \
