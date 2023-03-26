2547acb26b4f4a3307af1874c4b2e61fa0025182
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-26 18:03:34 +0600

    statix: fonts: introduce Manrope font
    Manrope font is an open-source modern sans-serif font family.
    Designed by Mikhail Sharanda in 2018-2021.
    
    Github: https://github.com/sharanda/manrope
    Italics fork: https://github.com/cssobral2013/manrope
    
    Change-Id: I4f84621f896f6f3c22a7bcf44cb50b97e898bf01
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/10521
    Reviewed-by: Sourajit Karmakar <sourajitk@statixos.com>
    Tested-by: Sourajit Karmakar <sourajitk@statixos.com>

--- config/overlay.mk
+++ config/overlay.mk
@@ -19,6 +19,7 @@ PRODUCT_PACKAGES += \
     FontDMSansOverlay \
     FontHarmonyOSSansOverlay \
     FontInterOverlay \
+    FontManropeOverlay \
     FontQuicksandOverlay \
     FontSwitzerOverlay
 
