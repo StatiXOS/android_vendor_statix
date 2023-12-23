a20c375de20cba5ffe4af7b5b30e622d814ebf83
Sourajit Karmakar <sourajitk@statixos.com>
2023-12-23 18:50:14 -0500

    statix: branding: Uprev to reflect qpr1 merge
    Change-Id: Id6217ce5fd105d2fb00435f293a33abf92377b2b
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/12480
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Reviewed-by: Ralph Chen <ralph950412@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -9,7 +9,7 @@ BUILD_DATE := $(shell date +%Y%m%d)
 
 ## Versioning System
 # Set all versions
-STATIX_BASE_VERSION := v7.0
+STATIX_BASE_VERSION := v7.1
 STATIX_PLATFORM_VERSION := 14
 
 # Use signing keys and don't print date & time in the final zip for official builds
