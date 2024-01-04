d0f5ec5282216383d236d5e4078324586da62719
Sourajit Karmakar <sourajitk@statixos.com>
2024-01-04 16:42:35 -0500

    statix: tasks: Clear out vendor_dlkm modules
    Change-Id: Ic4d93eb3265b2e58b5322034a55d50702a6060de
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/12640
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Reviewed-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>

--- build/tasks/CleanSpec.mk
+++ build/tasks/CleanSpec.mk
@@ -4,6 +4,7 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-$(warning Cleaning kernel objects.)
+$(warning Cleaning kernel and vendor_dlkm objects.)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/boot.img)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/kernel)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor_dlkm)
