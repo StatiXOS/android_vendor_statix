01972cd59c443e2ada8e89e930910abbdcb4a8d6
Vaisakh Murali <mvaisakh@statixos.com>
2025-09-25 19:12:03 +0200

    statix: move utils and pathmap back to statix
    Useful for non-qcom devices as well
    
    Change-Id: I1fe7d7009a109238c856150e3e6e3eb4440d2fcc
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/15535
    Tested-by: Dr. Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-by: Dr. Vaisakh Murali <mvaisakh@statixos.com>

--- config/common.mk
+++ config/common.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+include vendor/statix/build/core/pathmap.mk
+include vendor/statix/build/core/utils.mk
+
 # Conditionally call QCOM makefiles
 ifeq ($(PRODUCT_USES_QCOM_HARDWARE), true)
 include hardware/qcom-caf/common/build/core/ProductConfigQcom.mk
