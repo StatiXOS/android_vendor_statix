934f5f731866f47b164a071eab0a30bfbff4528f
ralph950412 <ralph950412@gmail.com>
2025-12-06 22:05:25 +0800

    statix: Set PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES
    Change-Id: Ie67f886cc35c454b07dbd7efbd0bd1fc85d138d7

--- config/packages.mk
+++ config/packages.mk
@@ -16,6 +16,8 @@ PRODUCT_PACKAGES += \
 # APEX
 DISABLE_DEXPREOPT_CHECK := true
 
+PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES=vendor/statix/build/target/product/security
+
 PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES=vendor/statix-prebuilts/apex/certificates
 
 PRODUCT_PACKAGES += \
